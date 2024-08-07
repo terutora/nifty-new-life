326f8f1d1f4adf4406dd91e68172a3802dfd0c79
黒川あゆみ <28osoblanco2020@gmail.com>
2024-08-07 03:54:53 +0000

    Squashed commit of the following
    commit 3dc4fe1c 
    Author: 28oso <28osoblanco2020@gmail.com> 
    Date: Wed Aug 07 2024 12:42:21 GMT+0900 (日本標準時) 
    
        fix: migration
    
    
    commit 00088bc5 
    Author: 28oso <28osoblanco2020@gmail.com> 
    Date: Wed Aug 07 2024 11:49:50 GMT+0900 (日本標準時) 
    
        fix: migration
    
    
    commit 7fc2b3a4 
    Author: 28oso <28osoblanco2020@gmail.com> 
    Date: Wed Aug 07 2024 10:21:29 GMT+0900 (日本標準時) 
    
        feat: Create Database

--- src/web/auth/models.py
+++ src/web/auth/models.py
@@ -7,6 +7,7 @@ class User(DB.Model, UserMixin):
     id = DB.Column(DB.Integer, primary_key=True)
     username = DB.Column(DB.String(128), unique=True)
     hashed_password = DB.Column(DB.String(256))
+    professional = DB.Column(DB.Integer, DB.ForeignKey("professional.professional_id"))
 
     def __init__(self, username: str, hashed_password: str):
         self.username = username
--- src/web/models.py
+++ src/web/models.py
@@ -1,5 +1,36 @@
+from datetime import datetime
+
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import TINYINT as Tinyint
 
 DB = SQLAlchemy()
 MIGRATE = Migrate()
+
+
+class Professional(DB.Model):
+    professional_id = DB.Column(DB.Integer, primary_key=True)
+    profession = DB.Column(DB.String(50))
+
+
+class Thread(DB.Model):
+    thread_id = DB.Column(DB.Integer, primary_key=True)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey("user.id"))
+    title = DB.Column(DB.String(50))
+    description = DB.Column(DB.String(50))
+    solve = DB.Column(Tinyint(1))
+    created_date = DB.Column(DB.DateTime, nullable=False, default=datetime.now)
+    updated_date = DB.Column(
+        DB.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
+    )
+
+
+class Answer(DB.Model):
+    answer_id = DB.Column(DB.Integer, primary_key=True)
+    thread_id = DB.Column(DB.Integer, DB.ForeignKey("thread.thread_id"))
+    user_id = DB.Column(DB.Integer)
+    description = DB.Column(DB.String(50))
+    created_date = DB.Column(DB.DateTime, nullable=False, default=datetime.now)
+    updated_date = DB.Column(
+        DB.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
+    )
